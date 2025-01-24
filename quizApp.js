ed8b05da263b56ab8d8087bd65de78cc6dba0deb
Vivek Rawal <vkrawal2004@gmail.com>
2025-01-24 05:24:24 +0000

    Change in some of the function and feture of QUiz

--- quizApp.js
+++ quizApp.js
@@ -23,8 +23,9 @@ let Ques = [
         question1: "which approach implemented by Event Bubbling",
         img: "js.png",
         options: {
-            right: "Bottom-Up approach",
+            
             wrong1: "Top-Down approach ",
+            right: "Bottom-Up approach",
             wrong2: "None of them"
         }
 
@@ -33,9 +34,10 @@ let Ques = [
         question1: "CSS stands for ?",
         img: "css.png",
         options: {
-            right: "Cascading Style sheet",
+           
             wrong1: "Cascading Style Standards",
-            wrong2: "Cascading Sheet Style"
+            wrong2: "Cascading Sheet Style",
+            right: "Cascading Style sheet"
         }
 
     }, {
@@ -43,8 +45,8 @@ let Ques = [
         question1: "Return type of Array?",
         img: "js.png",
         options: {
-            right: "Object",
             wrong1: "Array",
+            right: "Object",
             wrong2: "undefined"
         }
 
@@ -53,17 +55,17 @@ let Ques = [
         question1: "Memory Creation phase in JS execution store values ?",
         img: "js.png",
         options: {
-            right: "Yes",
-            wrong1: "No",
+            right: "NO",
+            wrong1: "YES",
         }
 
     }, {
         num: 7,
         question1: "why Promise is used in JS",
         img: "js.png",
-        options: {
-            right: "To perform Asynchronus Operation",
+        options: {                       
             wrong1: "To perform Synchronus Operation",
+            right: "To perform Asynchronus Operation",
             wrong2: "None of them"
         }
 
@@ -72,8 +74,8 @@ let Ques = [
         question1: "Return type of Object ",
         img: "js.png",
         options: {
-            right: "Object",
             wrong1: "Array",
+            right: "Object",
             wrong2: "Null"
         }
 
@@ -82,45 +84,98 @@ let Ques = [
         question1: ".copyWithIn() chnages Default Array",
         img: "js.png",
         options: {
-            right: "Yes",
-            wrong1: "No",
+            wrong1: "NO",
+            right: "YES"
+            
         }
     }, {
         num: 10,
         question1: "console.log('1'+1)",
         img: "js.png",
         options: {
-            right: "11",
             wrong1: "2",
+            right: "11",
             wrong2: "none"
         }
     }
 ]
 
+  let currentQuestionIndex = 0;
+  let timer=10
+  let score =0
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    start(currentQuestionIndex);
+  });
 
-document.addEventListener('DOMContentLoaded', () => {
+  // Function to display the current question
+  const start = (index) => {
 
-})
-
-const start = (x) => {
-    let image = document.getElementById('image').src=`${Ques[x].img}`
-    let number = document.getElementById('number').innerHTML = `${Ques[x].num}`
-    let bar = document.getElementById('file').value = `${Ques[x].num}`
-    let question = document.getElementById('questionDisplay').innerHTML = `${Ques[x].question1}`
-    let options = document.getElementById('option')
-    let sRed = document.getElementById('statues1')
-    sRed.style.backgroundColor = "red"
-
-    const optionKeys = Object.keys(Ques[x].options);
-    const optionValues = Object.values(Ques[x].options);
-
-    // Loop through the options and create elements for each
-    for (let i = 0; i < optionValues.length; i++) {
-        const optionDiv = document.createElement('div');
-        optionDiv.className = "option";
-        optionDiv.textContent = optionValues[i]; // Use the option value as text
-        options.appendChild(optionDiv); // Add to the options container
+    if (index >= Ques.length) {
+      document.getElementById("questionDisplay").innerHTML = `<h2>Quiz Completed!</h2><p>Your Score: ${score}/${Ques.length}</p>`;
+      let options = document.getElementById("option");
+      options.innerHTML = "";
+      if(score>8){
+        document.getElementById('statues3').style.backgroundColor='darkgreen'
+      }
+      else if(score<=8){
+        document.getElementById('statues2').style.backgroundColor='rgb(189, 189, 0)'
+      }
+      else{
+        document.getElementById('statues1').style.backgroundColor='darkred'
+      }
+      return;
     }
+    let bar = document.getElementById('file').value=`${Ques[index].num}`
+    let image = (document.getElementById("image").src = `${Ques[index].img}`);
+    let number = (document.getElementById("number").innerHTML = `${Ques[index].num}`);
+    let question = (document.getElementById(
+      "questionDisplay"
+    ).innerHTML = `${number}.${Ques[index].question1}`);
+    let options = document.getElementById("option");
+
+    // Clear previous options
+    options.innerHTML = "";
+  
+    // Create options dynamically
+    const optionValues = Object.values(Ques[index].options);
+  
+    optionValues.forEach((value) => {
+      const optionDiv = document.createElement("button");
+      optionDiv.className = "option";
+      optionDiv.textContent = value;
+  
+      // Add click listener to check the answer
+      optionDiv.addEventListener("click", (e) => {
+        if (value === Ques[index].options.right) {
+          score++; 
+        }
+        // Move to the next question
+        currentQuestionIndex++;
+        start(currentQuestionIndex);
+      });
+      options.appendChild(optionDiv);
+      countdown(timer)
+    });  
+    document.getElementById("start").style.visibility = "hidden";
     
-    document.getElementById('start').style.visibility='hidden'
-}
+  };
+  
+
+  function countdown(timer) {
+    if (window.timerInterval) {
+      clearInterval(window.timerInterval);
+    }
+  
+    window.timerInterval = setInterval(() => {
+      document.getElementById('timer').textContent = timer;
+  
+      if (timer === 0) {
+        clearInterval(window.timerInterval);
+        currentQuestionIndex++;
+        start(currentQuestionIndex);
+      } else {
+        timer--;
+      }
+    }, 1000);
+  }
